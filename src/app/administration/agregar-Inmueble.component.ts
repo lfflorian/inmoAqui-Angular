cefa1b304322b49866f1d7aa67a6d3f35a3359f8
Luis Fernando Florian <lflorian@mega.corp>
2017-12-22 16:48:34 -0600

    FormModule implementado

--- src/app/administration/agregar-Inmueble.component.ts
+++ src/app/administration/agregar-Inmueble.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, OnInit } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Inmueble } from '../Models/inmueble.model';
 
 @Component({
     selector: 'agregar-inmueble',
@@ -8,6 +10,21 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 })
 
 export class AgregarInmueble {
+    CreateForm = new FormGroup({
+        titulo: new FormControl(''),
+        descripcion: new FormControl(''),
+        tipoinmueble: new FormControl(''),
+        opcion: new FormControl(''),
+        nohabitaciones: new FormControl(''),
+        nobaños: new FormControl(''),
+        parqueos: new FormControl(''),
+        dimenciones: new FormControl(''),
+        departamento: new FormControl(''),
+        municipio: new FormControl(''),
+        zona: new FormControl(''),
+        colonia: new FormControl('')
+    });
+    
     TipoInmuebles: string[] = [
         "Casa",
         "Apartamento",
@@ -22,4 +39,24 @@ export class AgregarInmueble {
         "Renta"
     ]
 
+    onSubmit(CreateForm: FormGroup) {
+        /*inmo: Inmueble = {
+            id: 1,
+            titulo: CreateForm.value.titulo,
+            descripcion: CreateForm.value.descripcion,
+            tipoinmueble:  CreateForm.value.tipoinmueble,
+            opcion:  CreateForm.value.opcion,
+            nohabitaciones: CreateForm.value.nohabitaciones,
+            nobaños: CreateForm.value.nobaños,
+            parqueos: CreateForm.value.parqueos,
+            dimenciones: CreateForm.value.dimenciones,
+            departamento: CreateForm.value.departamento,
+            municipio: CreateForm.value.municipio,
+            zona: CreateForm.value.zona,
+            colonia: CreateForm.value.colonia 
+        };*/
+
+        console.log(CreateForm);
+    }
 }
+
