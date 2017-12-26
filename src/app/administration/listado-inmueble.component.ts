508571e3d83b97f3bda5f7334cc17189c01a97da
Luis Fernando Florian <lflorian@mega.corp>
2017-12-26 09:07:46 -0600

    opcion editar y eliminar agregaadas

--- src/app/administration/listado-inmueble.component.ts
+++ src/app/administration/listado-inmueble.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Inmueble } from '../Models/inmueble.model';
 
 @Component({
     selector: 'listado-inmueble',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
 })
 
 export class ListadoInmueble {
-    
+    inmuebles: Inmueble[] = [
+        new Inmueble(1,"Casa bonita", "esta casa es muiy bonita", "casa","venta", 2,2,2,190,"Guatemala", "Guatemala", "12", "El jaimico"),
+        new Inmueble(2,"Casa bonita", "esta casa es muiy bonita", "casa","venta", 2,2,2,190,"Guatemala", "Guatemala", "12", "El jaimico"),
+        new Inmueble(3,"Casa bonita", "esta casa es muiy bonita", "casa","venta", 2,2,2,190,"Guatemala", "Guatemala", "12", "El jaimico"),
+        new Inmueble(4,"Casa bonita", "esta casa es muiy bonita", "casa","venta", 2,2,2,190,"Guatemala", "Guatemala", "12", "El jaimico"),
+        new Inmueble(5,"Casa bonita", "esta casa es muiy bonita", "casa","venta", 2,2,2,190,"Guatemala", "Guatemala", "12", "El jaimico")
+    ];
+
+    refInmo: Inmueble;
+    editar(inmo: Inmueble) {
+        this.refInmo = inmo;
+        console.log("Editar inmueble con id: " + inmo.id);
+    }
+
+    eliminar(inmo: Inmueble) {
+        console.log("Eliminar inmueble con id: " + inmo.id);
+    }
 }
